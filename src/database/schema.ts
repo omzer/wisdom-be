a6b40e574ec2126cfbb55a10512a61ced4b441bb
o.sabri@freightos.com <o.sabri@freightos.com>
2024-05-11 01:49:11 +0300

    Add code for query users

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -2,5 +2,5 @@ import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 export const UsersTable = pgTable("users", {
   id: serial("id").primaryKey(),
-  username: varchar("name", { length: 12 }).notNull(),
+  nickname: varchar("nickname", { length: 12 }).notNull(),
 });
--- src/users/routes.ts
+++ src/users/routes.ts
@@ -1,11 +1,18 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { db } from "../index.ts";
 
-const routeSettings = {
-  prefix: "/user",
-  detail: { tags: ["User management"] },
+// Query users
+const queryUsers = ({ query }: any) => {
+  return db.query.UsersTable.findMany({ limit: query.limit, offset: query.offset, columns: { id: false } });
+};
+const queryUsersSchema = {
+  query: t.Object({ limit: t.Numeric({ minimum: 5 }), offset: t.Numeric({ minimum: 0 }) }),
 };
 
+// User routes
+const routeSettings = { prefix: "/user", detail: { tags: ["Users"] } };
 export const USER_ROUTES = new Elysia(routeSettings)
   .post("/sign-in", () => "Sign in")
   .post("/sign-up", () => "Sign up")
-  .post("/profile", () => "Profile");
+  .post("/profile", () => "Profile")
+  .get("/query", queryUsers, queryUsersSchema);
