bd2b169c4a3870e30d02e2e30d8c92874ae06c2b
o.sabri@freightos.com <o.sabri@freightos.com>
2024-05-12 00:14:56 +0300

    Changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { Elysia } from 'elysia';
 import swagger from '@elysiajs/swagger';
+import { dbConnect } from './utils/utils.ts';
 
 // Connect to the DB
+const sequelize = await dbConnect();
 
 // Serve APIs
 const app = new Elysia().use(swagger()).get('/', 'Hello').listen(4500);
-
-// Log status
-console.log(`App is running on port ${app.server?.port} 🦊`);
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,14 @@
+import { Sequelize } from 'sequelize';
+
 export const dbURL = () => process.env.DATABASE_URL as string;
+
+export const dbConnect = async () => {
+    const sequelize = new Sequelize(dbURL());
+    try {
+        await sequelize.authenticate();
+        console.log(`Connection to (${sequelize.getDatabaseName()}) established successfully 🔥`);
+    } catch (error) {
+        console.error('Unable to connect to the database 😭', error);
+    }
+    return sequelize;
+};
