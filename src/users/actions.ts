4098dd42f7009fb6f236590abc07b2f338604357
o.sabri@freightos.com <o.sabri@freightos.com>
2024-05-16 21:12:17 +0300

    Handle signup logic

--- src/users/actions.ts
+++ src/users/actions.ts
@@ -1,8 +1,16 @@
 import { db } from '../index.ts';
 
-export const signupUser = async ({ query, jwt }: any) => {
-    const user = await db.models.User.create({ username: query.username, password: query.password });
-    const token = await jwt.sign({ uid: user.toJSON().id });
-    console.log(token);
-    return user;
+export const signupUser = async ({ query, jwt, set }: any) => {
+    const [user, created] = await db.models.User.findOrCreate({
+        where: { username: query.username },
+        defaults: { username: query.username, password: query.password },
+    });
+
+    if (!created) {
+        set.status = 403;
+        return { message: 'User already exists in the DB' };
+    }
+
+    set.status = 201;
+    return { accessToken: `Bearer ${await jwt.sign({ uid: user.toJSON().id })}` };
 };
