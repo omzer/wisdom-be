79676b79999d66c20c9638275f104091bbf9cb76
o.sabri@freightos.com <o.sabri@freightos.com>
2024-05-17 15:19:44 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,24 @@ import { JWTConfigs, onBeforeMiddleware } from './common/utils.ts';
 export const db = {};
 
 // Start the server
-new Elysia()
-    // Elysia settings
-    .use(swagger())
-    .onBeforeHandle(onBeforeMiddleware)
-    // API routes
-    .use(USER_ROUTES)
-    .use(WISDOM_ROUTES)
-    .use(CATEGORY_ROUTES)
-    .use(jwt(JWTConfigs))
-    // Server port
-    .listen(3000);
+// const app = new Elysia()
+//     // Elysia settings
+//     .use(swagger())
+//     .onBeforeHandle(onBeforeMiddleware)
+//     // API routes
+//     .use(USER_ROUTES)
+//     .use(WISDOM_ROUTES)
+//     .use(CATEGORY_ROUTES)
+//     .use(jwt(JWTConfigs))
+//     // Server port
+//     .listen(3000);
 
 console.log('App is running 🔥');
+
+Bun.serve({
+    hostname: '::',
+    port: process.env.PORT ?? 3000,
+    fetch(request) {
+        return new Response('Welcome to Bun!');
+    },
+});
