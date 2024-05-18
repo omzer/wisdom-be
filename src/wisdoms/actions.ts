0971ed78aded9413795f6d87767ee0d7f7399fc0
o.sabri@freightos.com <o.sabri@freightos.com>
2024-05-18 14:19:14 +0300

    Support delete wisdom

--- src/wisdoms/actions.ts
+++ src/wisdoms/actions.ts
@@ -50,3 +50,15 @@ export const generateWisdom = async ({ query }: any) => {
     const result = await chat.sendMessage(topic);
     return result.response.text();
 };
+
+export const deleteWisdom = async ({ params, set }: any) => {
+    const { id } = params;
+    const wisdom: any = await db.models.Wisdom.findOne({ where: { id } });
+
+    // Wisdom does not exist
+    if (!wisdom) return (set.status = 404);
+    // User is not an author
+    if (wisdom.authorId !== set.userId) return (set.status = 401);
+
+    await wisdom.destroy();
+};
--- src/wisdoms/configs.ts
+++ src/wisdoms/configs.ts
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
 import { Elysia } from 'elysia';
 import { defaultIDConfigs, idParamValidation, limitOffsetValidation } from '../common/configs.ts';
-import { createWisdom, generateWisdom, updateWisdom, viewAll, viewWisdom } from './actions.ts';
+import { createWisdom, deleteWisdom, generateWisdom, updateWisdom, viewAll, viewWisdom } from './actions.ts';
 import { createWisdomValidation, generateWisdomValidation, updateWisdomValidation } from './actionsValidations.ts';
 
 // Model
@@ -15,7 +15,7 @@ const routeSettings = { prefix: '/wisdom', detail: { tags: ['Wisdom management']
 export const WISDOM_ROUTES = new Elysia(routeSettings)
     .get('/all', viewAll, limitOffsetValidation)
     .get('/view/:id', viewWisdom, idParamValidation)
-    .put('/delete/:id', updateWisdom, idParamValidation)
+    .delete('/delete/:id', deleteWisdom, idParamValidation)
     .post('/create', createWisdom, createWisdomValidation)
     .put('/update/:id', updateWisdom, updateWisdomValidation)
     .get('/generate', generateWisdom, generateWisdomValidation);
